e5e6299bf9a315f54ed978e23a650428c0341260
bspang <bspangler21@alumni.uwosh.edu>
2024-08-31 01:21:26 -0500

    chore: Add routing and page for fantasy football team

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { QueryClientProvider } from "react-query";
+import { QueryClient } from "react-query";
+import { initializeIcons } from "@fluentui/font-icons-mdl2";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+initializeIcons();
+
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<App />
+		</QueryClientProvider>
+	</StrictMode>
+);
---
+++ src/pages/FantasyFootballTeam.tsx
@@ -0,0 +1,9 @@
+const FantasyFootballTeam = () => {
+  return (
+    <div>
+      <h1>Fantasy Football</h1>
+    </div>
+  );
+}
+
+export default FantasyFootballTeam;
