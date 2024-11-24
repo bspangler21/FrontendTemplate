d782911b27eb72aa7ce35df2fcb6d8b52a5f7236
bspang <84486563+bspangler21@users.noreply.github.com>
2024-11-24 01:23:18 -0600

    V2 (#4)
    * Add player details page and navigation from cards
    
    * Remove unnecessary console log from PlayerDetails component

--- src/pages/Cards.tsx
+++ src/pages/Cards.tsx
@@ -3,18 +3,25 @@ import { FootballPlayer } from "../types/FootballPlayer";
 // import { TableHeader } from "../types/TableHeader";
 import utilStyles from "../styles/utilStyles.module.css";
 import cardStyles from "../styles/cardStyles.module.css";
+import { useNavigate } from "react-router-dom";
 
 const playersList: FootballPlayer[] = footballTeam;
 
 // const tableHeaders: TableHeader[] = [{ name: "Name" }, { name: "Position" }];
 
 const Cards = () => {
+	const nav = useNavigate();
+
 	return (
 		<>
 			<div className={utilStyles.container}>
 				<div className={cardStyles.horizontalContainer}>
 					{playersList.map((p) => (
-						<div key={p.id} className={cardStyles.card}>
+						<div
+							key={p.id}
+							className={cardStyles.card}
+							onClick={() => nav(`/player-detail/${p.id}`)}
+						>
 							<div className={cardStyles.w3CardContainer}>
 								<p>
 									{p.firstName} {p.lastName}
---
+++ src/pages/PlayerDetails.tsx
@@ -0,0 +1,41 @@
+import { useParams } from "react-router-dom";
+import { footballTeam } from "../data/footballTeam";
+import { FootballPlayer } from "../types/FootballPlayer";
+import { useEffect, useState } from "react";
+
+const playersList: FootballPlayer[] = footballTeam;
+// let player: FootballPlayer | undefined;
+
+function PlayerDetails() {
+	const { id } = useParams<{ id: string }>();
+	const [player, setPlayer] = useState<FootballPlayer | undefined>(undefined);
+
+	function getPlayerById(id: string) {
+		console.log("id", id);
+		return playersList.find((p) => p.id === id);
+	}
+
+	useEffect(() => {
+		// player = id ? getPlayerById(id) : playersList[0];
+		setPlayer(id ? getPlayerById(id) : playersList[0]);
+	}, [id]);
+
+	// if (isLoading) {
+	// 	return <p>Loading...</p>;
+	// }
+
+	// if (!player) {
+	// 	return <p>Player not found</p>;
+	// }
+
+	return (
+		<div>
+			<h1>
+				{player?.firstName} {player?.lastName}
+			</h1>
+			<p>{player?.position}</p>
+		</div>
+	);
+}
+
+export default PlayerDetails;
