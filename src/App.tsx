428b122738e919e654a751551d13489d87162ffd
BSPANGLER <bspangler21@alumni.uwosh.edu>
2024-11-29 23:09:00 -0600

    Remove unused Header and Footer imports from App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,8 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import FantasyFootballTeam from "./pages/FantasyFootballTeam";
-import Header from "./pageComponents/Header";
 import Home from "./pages/Home";
 import Placeholder from "./pages/Placeholder";
-import Footer from "./pageComponents/Footer";
 import Cards from "./pages/Cards";
 import PlayerDetails from "./pages/PlayerDetails";
 import Abstract from "./pages/Abstract";
