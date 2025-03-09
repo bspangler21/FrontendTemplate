ae96a689f8a5024b00c5184c7703b2dc74743472
bspang <bspangler21@alumni.uwosh.edu>
2025-03-09 00:24:34 -0600

    Add UserContext usage to Home component for current user information

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,10 @@
 import utilStyles from "../styles/utilStyles.module.css";
+import { useContext } from "react";
+import { UserContext } from "../main";
 
 const Home = () => {
+	const currentUser = useContext(UserContext);
+	console.log(`Current user: ${currentUser}`);
 	return (
 		<>
 			<div className={utilStyles.container}>
