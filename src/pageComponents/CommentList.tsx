f277b9dcd426290b489c8144dd9a000e857d2aa1
BSPANGLER <bspangler21@alumni.uwosh.edu>
2025-03-15 01:44:10 -0500

    Implement comment filtering and search functionality in Comments page

--- src/pageComponents/CommentList.tsx
+++ src/pageComponents/CommentList.tsx
@@ -1,11 +1,6 @@
 import { memo } from "react";
 import utilStyles from "../styles/utilStyles.module.css";
-import commentListStyles from "../styles/commentListStyles.module.css";
 import { ICommentItem } from "../interfaces/ICommentItem";
-import { ISearchBoxStyles, SearchBox } from "@fluentui/react/lib/SearchBox";
-import { Stack, IStackTokens } from "@fluentui/react/lib/Stack";
-
-
 
 interface CommentListProps {
 	comments: ICommentItem[];
@@ -14,7 +9,6 @@ interface CommentListProps {
 function CommentList({ comments }: CommentListProps) {
 	return (
 		<>
-			
 			<div className={utilStyles.container}>
 				<table className={utilStyles.table}>
 					<thead>
--- src/pages/Comments.tsx
+++ src/pages/Comments.tsx
@@ -1,30 +1,59 @@
-import { useEffect, useState } from "react";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useEffect, useRef, useState } from "react";
 import CommentList from "../pageComponents/CommentList";
 import { ICommentItem } from "../interfaces/ICommentItem";
 import utilStyles from "../styles/utilStyles.module.css";
-import { ISearchBoxStyles, IStackTokens, Stack } from "@fluentui/react";
+import commentStyles from "../styles/commentStyles.module.css";
+import {
+	ISearchBoxStyles,
+	IStackTokens,
+	SearchBox,
+	Stack,
+} from "@fluentui/react";
 
 const stackTokens: Partial<IStackTokens> = { childrenGap: 20 };
 const searchBoxStyles: Partial<ISearchBoxStyles> = { root: { width: 200 } };
 
 function Comments() {
 	const [comments, setComments] = useState<ICommentItem[]>([]);
+	const originalComments = useRef<ICommentItem[]>([]);
 
 	useEffect(() => {
 		fetch("https://jsonplaceholder.typicode.com/comments")
 			.then((response) => response.json())
-			.then((comments) => setComments(comments));
+			.then((comments) => {
+				setComments(comments);
+				originalComments.current = comments;
+			});
 	}, []);
+
+	const filterComments = (searchText: string | undefined) => {
+		console.log("searchText", searchText);
+		setComments(
+			originalComments.current.filter((comment) => {
+				if (!searchText) return true;
+				const searchLower = searchText.toLowerCase();
+        return (
+          comment.body.toLowerCase().includes(searchLower) || comment.name.toLowerCase().includes(searchLower)
+        )
+			})
+		);
+	};
+
+	const resetComments = () => {
+		setComments(originalComments.current);
+	};
 
 	return (
 		<div className={utilStyles.container}>
-      <Stack tokens={stackTokens}></Stack>
+			<Stack tokens={stackTokens}></Stack>
 			<SearchBox
-				className={commentListStyles.SearchBox}
+				className={commentStyles.SearchBox}
 				// styles={searchBoxStyles}
 				placeholder="Search"
 				// onSearch={(newValue) => console.log("value is " + newValue)}
-        onChange={(e) => }
+				onChange={(_, newValue: string) => filterComments(newValue)}
+				onClear={resetComments}
 			/>
 			<CommentList comments={comments} />
 			<br></br>
