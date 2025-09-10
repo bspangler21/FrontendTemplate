604c3e783fe5bf7ac84f827cdca2d5e81006b615
Brett Spangler <bspangler@capital-data.com>
2025-09-10 16:02:21 -0500

    Update Form component to include multiple input fields and enhance layout styles in form.module.css

--- src/pages/Form.tsx
+++ src/pages/Form.tsx
@@ -1,6 +1,18 @@
 import formStyles from "../styles/form.module.css";
 
 function Form() {
-	return <div className={formStyles.responsiveForm}>Form Page</div>;
+	return (
+		<div className={formStyles.responsiveForm}>
+			<input type="text" placeholder="Enter text" />
+			<input type="text" placeholder="Enter text" />
+			<input type="text" placeholder="Enter text" />
+			<input type="text" placeholder="Enter text" />
+			<input type="text" placeholder="Enter text" />
+			<input type="text" placeholder="Enter text" />
+			<input type="text" placeholder="Enter text" />
+			<input type="text" placeholder="Enter text" />
+			<input type="text" placeholder="Enter text" />
+		</div>
+	);
 }
 export default Form;
