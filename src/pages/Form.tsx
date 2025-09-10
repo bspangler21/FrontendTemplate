88b77c87870c81a7d554b147cb58281361a5ca27
Brett Spangler <bspangler@capital-data.com>
2025-09-10 16:47:57 -0500

    Spaced text inputs

--- src/pages/Form.tsx
+++ src/pages/Form.tsx
@@ -1,17 +1,98 @@
 import formStyles from "../styles/form.module.css";
+import utilStyles from "../styles/util.module.css";
 
 function Form() {
 	return (
-		<div className={formStyles.responsiveForm}>
-			<input type="text" placeholder="Enter text" />
-			<input type="text" placeholder="Enter text" />
-			<input type="text" placeholder="Enter text" />
-			<input type="text" placeholder="Enter text" />
-			<input type="text" placeholder="Enter text" />
-			<input type="text" placeholder="Enter text" />
-			<input type="text" placeholder="Enter text" />
-			<input type="text" placeholder="Enter text" />
-			<input type="text" placeholder="Enter text" />
+		<div className={formStyles.form}>
+			<div className={formStyles.formRow}>
+				<div className={formStyles.formInputContainer}>
+					<input
+						type="text"
+						placeholder="Enter text"
+						className={utilStyles.textInput}
+					/>
+				</div>
+
+				<div className={formStyles.formInputContainer}>
+					<input
+						type="text"
+						placeholder="Enter text"
+						className={utilStyles.textInput}
+					/>
+				</div>
+
+				<div className={formStyles.formInputContainer}>
+					<input
+						type="text"
+						placeholder="Enter text"
+						className={utilStyles.textInput}
+					/>
+				</div>
+
+				<div className={formStyles.formInputContainer}>
+					<input
+						type="text"
+						placeholder="Enter text"
+						className={utilStyles.textInput}
+					/>
+				</div>
+				<div className={formStyles.formInputContainer}>
+					<input
+						type="text"
+						placeholder="Enter text"
+						className={utilStyles.textInput}
+					/>
+				</div>
+				<div className={formStyles.formInputContainer}>
+					<input
+						type="text"
+						placeholder="Enter text"
+						className={utilStyles.textInput}
+					/>
+				</div>
+				<div className={formStyles.formInputContainer}>
+					<input
+						type="text"
+						placeholder="Enter text"
+						className={utilStyles.textInput}
+					/>
+				</div>
+				<div className={formStyles.formInputContainer}>
+					<input
+						type="text"
+						placeholder="Enter text"
+						className={utilStyles.textInput}
+					/>
+				</div>
+				<div className={formStyles.formInputContainer}>
+					<input
+						type="text"
+						placeholder="Enter text"
+						className={utilStyles.textInput}
+					/>
+				</div>
+				<div className={formStyles.formInputContainer}>
+					<input
+						type="text"
+						placeholder="Enter text"
+						className={utilStyles.textInput}
+					/>
+				</div>
+				<div className={formStyles.formInputContainer}>
+					<input
+						type="text"
+						placeholder="Enter text"
+						className={utilStyles.textInput}
+					/>
+				</div>
+				<div className={formStyles.formInputContainer}>
+					<input
+						type="text"
+						placeholder="Enter text"
+						className={utilStyles.textInput}
+					/>
+				</div>
+			</div>
 		</div>
 	);
 }
