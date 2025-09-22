44c69b9bffdc233899a192d4ca07772127e9a942
Brett Spangler <bspangler@capital-data.com>
2025-09-22 10:30:35 -0500

    Refactor Form component: streamline input structure and improve layout consistency

--- src/pages/Form.tsx
+++ src/pages/Form.tsx
@@ -3,102 +3,44 @@ import utilStyles from "../styles/util.module.css";
 
 function Form() {
 	return (
-		<div className={formStyles.form}>
-			<div className={formStyles.formRow}>
-				<div className={formStyles.formInputContainer}>
-					<label className={utilStyles.label}>Label</label>
-					<input
-						type="text"
-						placeholder="Enter text"
-						className={utilStyles.textInput}
-					/>
-				</div>
+		<div className={utilStyles.container}>
+			<div className={formStyles.form}>
+				<div className={formStyles.formRow}>
+					<div className={formStyles.formInputContainer}>
+						<label className={utilStyles.label}>Label</label>
+						<input
+							type="text"
+							placeholder="Enter text"
+							className={utilStyles.textInput}
+						/>
+					</div>
 
-				<div className={formStyles.formInputContainer}>
-					<label className={utilStyles.label}>Label</label>
-					<select className={utilStyles.textInput}>
-						<option value="">Select an option</option>
-						<option value="option1">Option 1</option>
-						<option value="option2">Option 2</option>
-					</select>
-				</div>
+					<div className={formStyles.formInputContainer}>
+						<label className={utilStyles.label}>Label</label>
+						<select className={utilStyles.textInput}>
+							<option value="">Select an option</option>
+							<option value="option1">Option 1</option>
+							<option value="option2">Option 2</option>
+						</select>
+					</div>
 
-				<div className={formStyles.formInputContainer}>
-					<label className={utilStyles.label}>Label</label>
-					<input
-						type="text"
-						placeholder="Enter text"
-						className={utilStyles.textInput}
-					/>
-				</div>
+					<div className={formStyles.formInputContainer}>
+						<label className={utilStyles.label}>Label</label>
+						<input
+							type="text"
+							placeholder="Enter text"
+							className={utilStyles.textInput}
+						/>
+					</div>
 
-				<div className={formStyles.formInputContainer}>
-					<label className={utilStyles.label}>Label</label>
-					<input
-						type="text"
-						placeholder="Enter text"
-						className={utilStyles.textInput}
-					/>
-				</div>
-			</div>
-			<div className={formStyles.formRow}>
-				<div className={formStyles.formInputContainer}>
-					<label className={utilStyles.label}>Label</label>
-					<input
-						type="text"
-						placeholder="Enter text"
-						className={utilStyles.textInput}
-					/>
-				</div>
-				<div className={formStyles.formInputContainer}>
-					<label className={utilStyles.label}>Label</label>
-					<input
-						type="text"
-						placeholder="Enter text"
-						className={utilStyles.textInput}
-					/>
-				</div>
-				<div className={formStyles.formInputContainer}>
-					<label className={utilStyles.label}>Label</label>
-					<input
-						type="text"
-						placeholder="Enter text"
-						className={utilStyles.textInput}
-					/>
-				</div>
-				<div className={formStyles.formInputContainer}>
-					<label className={utilStyles.label}>Label</label>
-					<input
-						type="text"
-						placeholder="Enter text"
-						className={utilStyles.textInput}
-					/>
-				</div>
-			</div>
-			<div className={formStyles.formRow}>
-				<div className={formStyles.formInputContainer}>
-					<label className={utilStyles.label}>Label</label>
-					<input
-						type="text"
-						placeholder="Enter text"
-						className={utilStyles.textInput}
-					/>
-				</div>
-				<div className={formStyles.formInputContainer}>
-					<label className={utilStyles.label}>Label</label>
-					<input
-						type="text"
-						placeholder="Enter text"
-						className={utilStyles.textInput}
-					/>
-				</div>
-				<div className={formStyles.formInputContainer}>
-					<label className={utilStyles.label}>Label</label>
-					<input
-						type="text"
-						placeholder="Enter text"
-						className={utilStyles.textInput}
-					/>
+					<div className={formStyles.formInputContainer}>
+						<label className={utilStyles.label}>Label</label>
+						<input
+							type="text"
+							placeholder="Enter text"
+							className={utilStyles.textInput}
+						/>
+					</div>
 				</div>
 				<div className={formStyles.formRow}>
 					<div className={formStyles.formInputContainer}>
@@ -116,6 +58,66 @@ function Form() {
 							placeholder="Enter text"
 							className={utilStyles.textInput}
 						/>
+					</div>
+					<div className={formStyles.formInputContainer}>
+						<label className={utilStyles.label}>Label</label>
+						<input
+							type="text"
+							placeholder="Enter text"
+							className={utilStyles.textInput}
+						/>
+					</div>
+					<div className={formStyles.formInputContainer}>
+						<label className={utilStyles.label}>Label</label>
+						<input
+							type="text"
+							placeholder="Enter text"
+							className={utilStyles.textInput}
+						/>
+					</div>
+				</div>
+				<div className={formStyles.formRow}>
+					<div className={formStyles.formInputContainer}>
+						<label className={utilStyles.label}>Label</label>
+						<input
+							type="text"
+							placeholder="Enter text"
+							className={utilStyles.textInput}
+						/>
+					</div>
+					<div className={formStyles.formInputContainer}>
+						<label className={utilStyles.label}>Label</label>
+						<input
+							type="text"
+							placeholder="Enter text"
+							className={utilStyles.textInput}
+						/>
+					</div>
+					<div className={formStyles.formInputContainer}>
+						<label className={utilStyles.label}>Label</label>
+						<input
+							type="text"
+							placeholder="Enter text"
+							className={utilStyles.textInput}
+						/>
+					</div>
+					<div className={formStyles.formRow}>
+						<div className={formStyles.formInputContainer}>
+							<label className={utilStyles.label}>Label</label>
+							<input
+								type="text"
+								placeholder="Enter text"
+								className={utilStyles.textInput}
+							/>
+						</div>
+						<div className={formStyles.formInputContainer}>
+							<label className={utilStyles.label}>Label</label>
+							<input
+								type="text"
+								placeholder="Enter text"
+								className={utilStyles.textInput}
+							/>
+						</div>
 					</div>
 				</div>
 			</div>
